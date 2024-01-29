6ece1858b0f0509bb5e4271ecc60be37379ed061
SantiGalvan <santiagogalvancolorado@gmail.com>
2024-01-29 16:55:40 +0100

    add emails

--- js/script.js
+++ js/script.js
@@ -1,6 +1,25 @@
 console.log('Vue OK', Vue);
 
+const endpoint = 'https://flynn.boolean.careers/exercises/api/random/mail';
+
 // Attivo Vue
 const { createApp } = Vue;
-const app = createApp({});
+const app = createApp({
+    data: () => ({
+        emails: []
+    }),
+    methods: {
+        getRandomEmail() {
+            for (let i = 0; i < 10; i++) {
+                axios.get(endpoint).then((res) => {
+                    this.emails.push(res.data.response)
+                    console.log(this.emails)
+                })
+            }
+        }
+    },
+    created() {
+        this.getRandomEmail()
+    }
+});
 app.mount('#root');
